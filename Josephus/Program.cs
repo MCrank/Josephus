2f4eb1e8d6ae16033eb0a393120a05857a568b88
MCrank <marco_mcse@hotmail.com>
2019-03-09 14:09:15 -0600

    Working code

--- Josephus/Program.cs
+++ Josephus/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Josephus
 {
@@ -6,7 +7,53 @@ namespace Josephus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter number of people");
+            var numberOfPeople = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter killing interval");
+            var killingInterval = Convert.ToInt32(Console.ReadLine());
+
+            List<int> People = new List<int>(CreatePeople(numberOfPeople));
+
+            LastPersonStanding(People, killingInterval);
+
+            Console.ReadLine();
+        }
+
+        public static void LastPersonStanding(List<int> people, int killingInterval)
+        {
+            //int index = 1;
+            do
+            {
+                int killingCounter = 1;
+                for (int i = 0; i < people.Count; i++)
+                {
+                    if (killingCounter == killingInterval)
+                    {
+                        Console.WriteLine($"killed person at {people[0]}");
+                        people.RemoveAt(0);
+                        killingCounter = 1;
+                    }
+                    else
+                    {
+                        //int x = people.Count - 1;
+                        people.Add(people[0]);
+                        people.RemoveAt(0);
+                        killingCounter++;
+                        i--;
+                    }
+                }
+            } while (people.Count > 1);
+            Console.WriteLine($"Last person standing {people[0]}!");
+        }
+
+        public static List<int> CreatePeople(int numberOfPeople)
+        {
+            List<int> tempList = new List<int>();
+            for (int i = 1; i <= numberOfPeople; i++)
+            {
+                tempList.Add(i);
+            }
+            return tempList;
         }
     }
 }
